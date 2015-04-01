07fc290f50ae973acac01828060f3e2d60e4ac3a
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2015-04-01 13:08:21 +0100

    Library project created

---
+++ PAAppColors/PAAppColors/PAAppColors.h
@@ -0,0 +1,13 @@
+//
+//  PAAppColors.h
+//  PAAppColors
+//
+//  Created by Pau on 01/04/2015.
+//  Copyright (c) 2015 PabloAlejandro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAAppColors : NSObject
+
+@end
---
+++ PAAppColors/PAAppColors/PAAppColors.m
@@ -0,0 +1,13 @@
+//
+//  PAAppColors.m
+//  PAAppColors
+//
+//  Created by Pau on 01/04/2015.
+//  Copyright (c) 2015 PabloAlejandro. All rights reserved.
+//
+
+#import "PAAppColors.h"
+
+@implementation PAAppColors
+
+@end
